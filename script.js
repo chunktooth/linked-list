27246453eb0da6f858cf7fa4c84a159a0058f7de
chunktooth <om.saetehn@gmail.com>
2017-12-11 16:33:05 -0700

    Add toggle for Read button

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-
-  
-$('.enter-btn').on('click', function () {
-  event.preventDefault();
-  var siteInput = $('.title-input').val();
-  $('.site-title').text(siteInput);
+$('.enter-btn').on('click', function() {
+  var titleInput = $('.title-input').val();
   var linkInput = $('.link-input').val();
+  event.preventDefault();
+  $('.site-title').text(titleInput);
   $('.site-url').text(linkInput);
+  console.log(titleInput + linkInput);
   $(':input').val('');
 });
 
-$('.site-url').on('click', function () {
-  ('.read-btn').addClass('read').prop('disabled');
-});
+  $('.read').on('click', function() {
+    $(this).toggleClass('read-red')
+  });
+
+
+// $('.site-url').on('click', function () {
+//   ('.read-btn').addClass('read').prop('disabled');
+// });
 
 // change .site-url from on 'click' to 'keyup'
