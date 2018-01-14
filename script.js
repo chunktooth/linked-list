4fab961709901bca0619d2a3711c214a0311e307
chunktooth <om.saetehn@gmail.com>
2018-01-14 00:12:44 -0700

    Attempting read and unread counter TBC

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function enableButton(e) {
     if(titleInput.val() && linkInput.val()) {
         $('.enter-btn').attr('disabled', false);
         $('.required-input').text('');
-        validateLink(linkInput);
+        validateLink();
     } else {
         $('.enter-btn').attr('disabled', true);
         $('.required-input').text('Please fill out both fields');
@@ -37,13 +37,12 @@ function addToCard(e){
     $('.required-input').text('');
 }
 
-function validateLink(linkInput) {
-    var linkInput = linkInput.val();
-    var emailRegex =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-    if(emailRegex.test(linkInput) === true) {
+function validateLink() {
+    var linkInput = $('.link-input').val();
+    var linkRegex =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    if(linkRegex.test(linkInput) === true) {
         $('.required-input').text('');
         $('enter-btn').attr('disabled', false);
-        return true;
     } else {
         $('.required-input').text('Please enter valid URL');
         $('.enter-btn').attr('disabled', true);
@@ -51,9 +50,16 @@ function validateLink(linkInput) {
 }
 
 function readCard() {
+    $(this).toggleClass('read-in-red');
     $(this).parent().toggleClass('marked-read');
+    var readCards = $('.read-in-red');
+    for(var i = 0; i < readCards.length; i++) {
+        console.log(readCards[i]);
+    }
+    // $('#read-counter').text(readCounter);
 }
 
 function deleteCard() {
     $(this).parent().remove();
 }
+
